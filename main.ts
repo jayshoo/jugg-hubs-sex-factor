bb91ecca0f8e1cec2690ca1af54ee911b393758f
Joshua McLean <jayshoo@gmail.com>
2021-10-14 23:32:20 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ let Decoder = new TextDecoder()
 let Encoder = new TextEncoder()
 
 async function sexFactor(name1: string, name2: string): Promise<Response> {
-  if (name1 == name2) return new Response(`Try lovin' someone else, ${name1}!`)
+  if (name1.toLowerCase() == name2.toLowerCase()) return new Response(`Try lovin' someone else, ${name1}!`)
   
-  let data1 = Encoder.encode(name1)
-  let data2 = Encoder.encode(name2)
+  let data1 = Encoder.encode(name1.toLowerCase())
+  let data2 = Encoder.encode(name2.toLowerCase())
   let hash1 = await crypto.subtle.digest('SHA-256', data1)
   let hash2 = await crypto.subtle.digest('SHA-256', data2)
   let view1 = new DataView(hash1)
@@ -17,6 +17,7 @@ async function sexFactor(name1: string, name2: string): Promise<Response> {
   let n = x - 16 // -16..111
 
   let answer = `${name1} and ${name2} have ${n}% sex factor!`
+  if (n >= 100) answer += ` CurseLit HOT HOT HOT!!! CurseLit`
   
   return new Response(answer)
 }
